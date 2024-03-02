18b89a913414c2d944fae8bb2ff995f8a140ea16
Kenneth Blaney <kenneth.r.blaney@gmail.com>
2024-03-02 16:25:22 -0500

    added all animals

--- game/utils/player_data.py
+++ game/utils/player_data.py
@@ -65,7 +65,312 @@ class PlayerData:
              "sonar": False,
              }
 
-    start_candidates = [shark, octopus, squid]
+    cuttlefish = {"name": "cuttlefish",
+                  "fins": True,
+                  "gills": True,
+                  "lungs": False,
+                  "venom": True,
+                  "shell": False,
+                  "claws": False,
+                  "elec": False,
+                  "teeth": False,
+                  "acid": False,
+                  "tentacles": True,
+                  "spines": False,
+                  "sonar": False,
+                  }
+
+    jellyfish = {"name": "jellyfish",
+                 "fins": False,
+                 "gills": False,
+                 "lungs": False,
+                 "venom": True,
+                 "shell": False,
+                 "claws": False,
+                 "elec": False,
+                 "teeth": False,
+                 "acid": True,
+                 "tentacles": True,
+                 "spines": False,
+                 "sonar": False,
+                 }
+
+    fish = {"name": "fish",
+            "fins": True,
+            "gills": True,
+            "lungs": False,
+            "venom": False,
+            "shell": False,
+            "claws": False,
+            "elec": False,
+            "teeth": False,
+            "acid": False,
+            "tentacles": False,
+            "spines": False,
+            "sonar": False,
+            }
+
+    urchin = {"name": "urchin",
+              "fins": False,
+              "gills": False,
+              "lungs": False,
+              "venom": True,
+              "shell": True,
+              "claws": False,
+              "elec": False,
+              "teeth": False,
+              "acid": False,
+              "tentacles": False,
+              "spines": True,
+              "sonar": False,
+              }
+
+    starfish = {"name": "starfish",
+                "fins": False,
+                "gills": False,
+                "lungs": False,
+                "venom": False,
+                "shell": True,
+                "claws": False,
+                "elec": False,
+                "teeth": False,
+                "acid": False,
+                "tentacles": False,
+                "spines": True,
+                "sonar": False,
+                }
+
+    dolphin = {"name": "dolphin",
+               "fins": True,
+               "gills": False,
+               "lungs": True,
+               "venom": False,
+               "shell": False,
+               "claws": False,
+               "elec": False,
+               "teeth": True,
+               "acid": False,
+               "tentacles": False,
+               "spines": False,
+               "sonar": True,
+               }
+
+    whale = {"name": "whale",
+             "fins": True,
+             "gills": False,
+             "lungs": True,
+             "venom": False,
+             "shell": False,
+             "claws": False,
+             "elec": False,
+             "teeth": False,
+             "acid": False,
+             "tentacles": False,
+             "spines": False,
+             "sonar": True,
+             }
+
+    clam = {"name": "clam",
+            "fins": False,
+            "gills": False,
+            "lungs": False,
+            "venom": False,
+            "shell": True,
+            "claws": False,
+            "elec": False,
+            "teeth": False,
+            "acid": False,
+            "tentacles": False,
+            "spines": False,
+            "sonar": False,
+            }
+
+    seal = {"name": "seal",
+            "fins": True,
+            "gills": False,
+            "lungs": True,
+            "venom": False,
+            "shell": False,
+            "claws": True,
+            "elec": False,
+            "teeth": False,
+            "acid": False,
+            "tentacles": False,
+            "spines": False,
+            "sonar": False,
+            }
+
+    manatee = {"name": "manatee",
+               "fins": True,
+               "gills": False,
+               "lungs": True,
+               "venom": False,
+               "shell": False,
+               "claws": False,
+               "elec": False,
+               "teeth": False,
+               "acid": False,
+               "tentacles": False,
+               "spines": False,
+               "sonar": False,
+               }
+
+    walrus = {"name": "walrus",
+              "fins": True,
+              "gills": False,
+              "lungs": True,
+              "venom": False,
+              "shell": False,
+              "claws": False,
+              "elec": False,
+              "teeth": True,
+              "acid": False,
+              "tentacles": False,
+              "spines": False,
+              "sonar": False,
+              }
+
+    shrimp = {"name": "shrimp",
+              "fins": True,
+              "gills": True,
+              "lungs": False,
+              "venom": False,
+              "shell": True,
+              "claws": False,
+              "elec": False,
+              "teeth": False,
+              "acid": False,
+              "tentacles": False,
+              "spines": False,
+              "sonar": False,
+              }
+
+    crab = {"name": "crab",
+            "fins": False,
+            "gills": True,
+            "lungs": False,
+            "venom": False,
+            "shell": True,
+            "claws": True,
+            "elec": False,
+            "teeth": False,
+            "acid": False,
+            "tentacles": False,
+            "spines": False,
+            "sonar": False,
+            }
+
+    lobster = {"name": "walrus",
+               "fins": True,
+               "gills": True,
+               "lungs": False,
+               "venom": False,
+               "shell": True,
+               "claws": True,
+               "elec": False,
+               "teeth": False,
+               "acid": False,
+               "tentacles": False,
+               "spines": False,
+               "sonar": False,
+               }
+
+    turtle = {"name": "turtle",
+              "fins": False,
+              "gills": False,
+              "lungs": True,
+              "venom": False,
+              "shell": True,
+              "claws": False,
+              "elec": False,
+              "teeth": False,
+              "acid": False,
+              "tentacles": False,
+              "spines": False,
+              "sonar": False,
+              }
+
+    sea_turtle = {"name": "sea turtle",
+                  "fins": True,
+                  "gills": False,
+                  "lungs": True,
+                  "venom": False,
+                  "shell": True,
+                  "claws": False,
+                  "elec": False,
+                  "teeth": False,
+                  "acid": False,
+                  "tentacles": False,
+                  "spines": False,
+                  "sonar": False,
+                  }
+
+    eel = {"name": "eel",
+           "fins": True,
+           "gills": True,
+           "lungs": False,
+           "venom": False,
+           "shell": False,
+           "claws": False,
+           "elec": True,
+           "teeth": False,
+           "acid": False,
+           "tentacles": False,
+           "spines": False,
+           "sonar": False,
+           }
+
+    sea_cucumber = {"name": "sea cucumber",
+                    "fins": False,
+                    "gills": False,
+                    "lungs": False,
+                    "venom": False,
+                    "shell": False,
+                    "claws": False,
+                    "elec": False,
+                    "teeth": False,
+                    "acid": True,
+                    "tentacles": True,
+                    "spines": False,
+                    "sonar": False,
+                    }
+
+    sting_ray = {"name": "sting ray",
+                 "fins": True,
+                 "gills": True,
+                 "lungs": False,
+                 "venom": True,
+                 "shell": False,
+                 "claws": False,
+                 "elec": False,
+                 "teeth": False,
+                 "acid": False,
+                 "tentacles": False,
+                 "spines": True,
+                 "sonar": False,
+                 }
+
+    sea_sponge = {"name": "sea sponge",
+                  "fins": False,
+                  "gills": False,
+                  "lungs": False,
+                  "venom": False,
+                  "shell": False,
+                  "claws": False,
+                  "elec": False,
+                  "teeth": False,
+                  "acid": False,
+                  "tentacles": False,
+                  "spines": False,
+                  "sonar": False,
+                  }
+
+    start_candidates = [clam, crab, cuttlefish, dolphin, eel, fish, jellyfish, lobster, manatee, octopus,
+                        sea_cucumber, sea_sponge, sea_turtle, seal, shark, shrimp, squid, starfish, sting_ray, turtle,
+                        urchin, walrus, whale]
+
+    jellyfish, sea_cucumber
+
     candidates = start_candidates
     candidate_names = [item['name'] for item in candidates]
 
@@ -140,3 +445,15 @@ class PlayerData:
                     break
             if eliminated:
                 self.qualities[quality] = target
+
+    def test_validity_of_candidates(self):
+        anon_candidates = []
+        for candidate in self.candidates:
+            del candidate['name']
+            anon_candidates.append(candidate)
+        for i, candidate in enumerate(anon_candidates):
+            for j, candidate2 in enumerate(anon_candidates):
+                if i == j:
+                    continue
+                if candidate == candidate2:
+                    print(f"Candidate collision on {i} and {j}")
